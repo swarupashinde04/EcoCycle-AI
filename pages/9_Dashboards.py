c29d2db0a9dfaa195f0d094c61ba915aea4c2191
Swarupa Shinde <swarupa8387@gmail.com>
2026-08-07 17:40:44 +0530

    Updated

--- pages/9_Dashboards.py
+++ pages/9_Dashboards.py
@@ -8,7 +8,7 @@ st.set_page_config(
     layout="wide"
 )
 
-st.title("📊 EcoCycle AI Dashboard")
+st.title("📊 GreenTick Dashboard")
 st.write("Overview of the complete recycling ecosystem.")
 
 st.divider()
